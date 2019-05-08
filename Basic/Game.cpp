9883db11e1f119f27456ec1c74fbadc2dcc552ac
calvarez37 <50217880+calvarez37@users.noreply.github.com>
2019-05-07 21:47:48 -0700

    Update Game.cpp

--- Basic/Game.cpp
+++ Basic/Game.cpp
@@ -6,11 +6,36 @@
 
 Game::Game(){
 
-	// Some cross-platform compatimility stuff
+// Some cross-platform compatimility stuff
 
-	//const char* shroomFileName;
-    //const char* fireballFileName;
+    const char* frogFileName;
+    const char* purpleTileFileName;
+    const char* carFileName;
+    
+    
+    // In Windows (Visual Studio only) the image files are found in the enclosing folder in relation to the project
+    // In other environments the image files are in the same folder as the project
+    
+    #if defined WIN32
+    frogFileName = "../frog_NS.png" ;
+    purpleTileFileName = "../grass_tile.png";
+    carFileName = "../car_white.png";
+    #else
+    frogFileName = "frog_NS.png";
+    purpleTileFileName = "grass_tile.png";
+    carFileName = "car_white.png";
+    #endif
+	// In Windows (Visual Studio only) the image files are found in the enclosing folder in relation to the project
+	// In other environments the image files are in the same folder as the project
 
+
+    frog = new AnimatedRect(frogFileName, 1 , 3, 100, false, false, 0, 0, 0, 0.5);
+    car = new TexRect(carFileName, -0.4, -0.25, 0.1, 0.1);
+    purpleTile = new TexRect(purpleTileFileName, -1.1 , -0.4, 1.0, 0.5);
+    frogLives = new TexRect(frogFileName, 0.5, 0.5, 0.1, 0.1);
+    
+    frogAlive = true;
+    frog -> playLoop();
     
     setRate(1);
     start();
@@ -23,6 +48,10 @@ void Game::action()
 
 void Game::draw() const
 {
+	purpleTile -> draw(0.0);
+	car -> draw(0.0);
+	frog -> draw(-0.5);
+	frogLives -> draw(0.0);
     
 }
 
